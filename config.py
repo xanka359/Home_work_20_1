851230977ca0a7d216a38247b231bfca3259cc3f
Oksana Bondareva <xanka359@yandex.ru>
2024-04-15 23:26:45 +0300

    first commit for HW19

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+context = os.getenv('context', 'bstack')
+bstack_userName = os.getenv('bstack_userName', 'oksanabondareva_4dJCBH')
+bstack_accessKey = os.getenv('bstack_accessKey', 'EVxh5GDxEpqoJBmkGytA')
---
+++ tests/android_app/test_wikipedia.py
@@ -0,0 +1,15 @@
+import allure
+from appium.webdriver.common.appiumby import AppiumBy
+from selene import browser, have
+
+
+def test_search():
+
+    with allure.step('Type search'):
+        browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('Appium')
+
+    with allure.step('Verify content found'):
+        results = browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))
+        results.should(have.size_greater_than(0))
+        results.first.should(have.text('Appium'))
